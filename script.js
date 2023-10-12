065ca4876526d45b5bb0acde6d281e7387dc4cfc
hoangtran0935 <21110031@student.hcmute.edu.vn>
2023-10-12 09:15:24 +0700

    hhhh

---
+++ script.js
@@ -0,0 +1,12 @@
+var typed = new Typed(".typing", {
+    strings:["Web Designer","Web Developer","Content Creater","Talker"],
+    typeSpeed:100,
+    BackSpeed:60,
+    loop:true
+})
+var typeing = new Typed(".typing-text", {
+    strings:["I'm currently a third-year student at Ho Chi Minh City University of Technology and Education. This is my portfolio website. Thank you for visiting!"],
+    typeSpeed:100,
+    BackSpeed:60,
+    loop:true
+})
